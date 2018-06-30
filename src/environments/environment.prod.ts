9dd5f7e05209b11fe6d7f991676c54a9e8369ea3
Sandeep Bonam <deepu@Sandeeps-MacBook-Pro.local>
2018-06-30 15:44:25 -0400

    Added Firebase properties to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBR9qe5J7Rcn27UmWQQFEgCeVlj4yIv3RU",
+    authDomain: "organicshop-18573.firebaseapp.com",
+    databaseURL: "https://organicshop-18573.firebaseio.com",
+    projectId: "organicshop-18573",
+    storageBucket: "organicshop-18573.appspot.com",
+    messagingSenderId: "679033260104"
+  }
 };

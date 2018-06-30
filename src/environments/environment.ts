cab6439fd651a5cc0acc36883378647855331b5c
Sandeep Bonam <deepu@Sandeeps-MacBook-Pro.local>
2018-06-30 15:35:48 -0400

    Added Firebase properties to environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBR9qe5J7Rcn27UmWQQFEgCeVlj4yIv3RU",
+    authDomain: "organicshop-18573.firebaseapp.com",
+    databaseURL: "https://organicshop-18573.firebaseio.com",
+    projectId: "organicshop-18573",
+    storageBucket: "organicshop-18573.appspot.com",
+    messagingSenderId: "679033260104"
+  }
 };
 
 /*

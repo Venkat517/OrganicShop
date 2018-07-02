f2dc36b6803c5970f2f8f60519a4dfdb77cd0c93
Sandeep Bonam <deepu@Sandeeps-MacBook-Pro.local>
2018-07-02 12:58:51 -0400

    Added Routelinks to the dropdown menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 // import { AngularFireModule } from 'angularfire2';
+// import { AngularFireDatabaseModule } from 'angularfire2/database';
+// import { AngularFireAuthModule } from 'angularfire2/auth';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
@@ -15,6 +17,7 @@ import { OrderSuccessComponent } from './order-success/order-success.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+// import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -33,6 +36,10 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
   ],
   imports: [
     BrowserModule,
+    // AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireDatabaseModule,
+    // AngularFireAuthModule,
+
     NgbModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent},
